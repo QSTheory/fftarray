8f69dba610e530a368bef56b90177e5eb192f575
Gabriel Müller <g.mueller@iqo.uni-hannover.de>
2023-04-05 15:57:19 +0200

    Add quotes in FFTDimension __repr__

--- fftarray/fft_array.py
+++ fftarray/fft_array.py
@@ -916,7 +916,7 @@ class FFTDimension:
             f"Position space: min={self.pos_min}, middle={self.pos_middle}, max={self.pos_max}, extent={self.pos_extent}, d_pos={self.d_pos} \n\t " +
             f"Frequency space: min={self.freq_min}, middle={self.freq_middle}, max={self.freq_max}, extent={self.freq_extent}, d_pos={self.d_freq}"
         )
-        return f"FFTDimension with name {self.name} on backend {self.default_tlib} evaluated {evaluated} with the following properties:\n{properties}"
+        return f"FFTDimension with name '{self.name}' on backend '{self.default_tlib}' evaluated {evaluated} with the following properties:\n{properties}"
 
     def set_default_tlib(self, tlib: TensorLib) -> FFTDimension:
         dim = copy(self)
