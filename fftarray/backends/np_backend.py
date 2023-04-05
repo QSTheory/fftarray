fc95068d14dd497c20dcf234674e59996236f92f
Christian Struckmann <struckmann@iqo.uni-hannover.de>
2023-04-05 10:34:08 +0200

    Format np_backend.py

--- fftarray/backends/np_backend.py
+++ fftarray/backends/np_backend.py
@@ -5,24 +5,26 @@ import warnings
 
 
 class NumpyTensorLib(TensorLib):
+
     def __init__(self, precision: PrecisionSpec = "default"):
         TensorLib.__init__(self, precision=precision)
         self.numpy_ufuncs = np
         self.array = np.array
 
-
     def fftn(self, values, precision: PrecisionSpec):
         transformed = np.fft.fftn(values)
         if precision == "fp32":
-            warnings.warn('numpy.fft.fftn always computes in double precision. Since precision was set to fp32 the result is automatically truncated.')
+            warnings.warn('numpy.fft.fftn always computes in double precision. \
+                Since precision was set to fp32 the result is automatically \
+                truncated.')
             return transformed.astype(np.complex64)
-        else:
-            return transformed
+        return transformed
 
     def ifftn(self, values, precision: PrecisionSpec):
         transformed = np.fft.ifftn(values)
         if precision == "fp32":
-            warnings.warn('numpy.fft.ifftn always computes in double precision. Since precision was set to fp32 the result is automatically truncated.')
+            warnings.warn('numpy.fft.ifftn always computes in double \
+                precision. Since precision was set to fp32 the result is \
+                automatically truncated.')
             return transformed.astype(np.complex64)
-        else:
-            return transformed
+        return transformed
