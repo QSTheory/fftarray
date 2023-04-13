b48a60eacc1d6331aa7560372797ceba9e47a0d8
Stefan Seckmeyer <seckmeyer@iqo.uni-hannvoer.de>
2023-04-13 11:09:53 +0200

    Expand documentation of UniformValue.

--- fftarray/helpers.py
+++ fftarray/helpers.py
@@ -8,7 +8,7 @@ T = TypeVar("T")
 # Helpers to reduce objects that should be same for all elements of a list
 #------------
 def reduce_equal(objects: Iterable[T], error_msg: str) -> T:
-    """Reduce the Iterable to a single instance while checking the assumption 
+    """Reduce the Iterable to a single instance while checking the assumption
     that all objects are the same.
     """
     def join_equal(a, b):
@@ -19,8 +19,16 @@ def reduce_equal(objects: Iterable[T], error_msg: str) -> T:
     return reduce(join_equal, objects)
 
 class UniformValue(Generic[T]):
-    """Allows the same reduction as "_reduce_equal" but when running through a 
-    loop.
+    """The idea of this class is that we often have the pattern that some
+    property needs to be the same (but in principal arbitrary) value
+    or None in all elements of a list.
+    Since it could be None we cannot just take the first element of this list
+    to get the value that all should adhere to.
+    Therefore this class takes on the value first set
+    and then checks on all consecutive sets that the value remained the same.
+
+    In essence this allows the same reduction as "_reduce_equal" but when
+    running through a loop.
     """
     is_set: bool
     value: Any
