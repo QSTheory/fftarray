3308c2db90a98233b81005b8b11eb6100b894ee1
Stefan Seckmeyer <seckmeyer@iqo.uni-hannvoer.de>
2024-02-01 15:10:39 +0100

    Merge pos_array and freq_array of FFTDimension into fft_array(space: Space)

--- fftarray/xr_helpers.py
+++ fftarray/xr_helpers.py
@@ -9,7 +9,7 @@ from .fft_array import FFTArray
 def as_xr_pos(arr: FFTArray) -> xr.DataArray:
     return xr.DataArray(
         np.array(arr.into(space="pos")),
-        coords = {dim.name: np.array(dim.pos_array()) for dim in arr.dims},
+        coords = {dim.name: np.array(dim.fft_array(space="pos")) for dim in arr.dims},
         # TODO These in the attributes somehow crash where with a pickle error.
         # attrs = _xr_attribs(arr),
     )
@@ -17,7 +17,7 @@ def as_xr_pos(arr: FFTArray) -> xr.DataArray:
 def as_xr_freq(arr: FFTArray) -> xr.DataArray:
     return xr.DataArray(
         np.array(arr.into(space="freq")),
-        coords = {dim.name: np.array(dim.freq_array()) for dim in arr.dims},
+        coords = {dim.name: np.array(dim.fft_array(space="freq")) for dim in arr.dims},
         # attrs = _xr_attribs(arr),
     )
 
@@ -26,14 +26,14 @@ def as_xr_dataset(arr: FFTArray) -> xr.Dataset:
             "pos": xr.DataArray(
                 np.array(arr.into(space="pos")),
                 coords = {
-                    f"{dim.name}_pos": np.array(dim.pos_array())
+                    f"{dim.name}_pos": np.array(dim.fft_array(space="pos"))
                     for dim in arr.dims
                 }
             ),
             "freq":  xr.DataArray(
                 np.array(arr.into(space="freq")),
                 coords = {
-                    f"{dim.name}_freq": np.array(dim.freq_array())
+                    f"{dim.name}_freq": np.array(dim.fft_array(space="freq"))
                     for dim in arr.dims
                 }
             ),
