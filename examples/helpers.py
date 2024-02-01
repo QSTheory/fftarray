425e72f011084eed104f0f6e41f249b782311874
Gabriel Müller <g.mueller@iqo.uni-hannover.de>
2024-02-01 11:22:47 +0100

    Add ignore flags to pass mypy examples

--- examples/helpers.py
+++ examples/helpers.py
@@ -15,12 +15,12 @@ def plt_fftarray(
         p_pos = figure(width=450, height=400, x_axis_label = f"{dim.name} pos coordinate", min_border=50)
         p_pos.line(np.array(dim.pos_array()), np.real(arr.pos_array().values), line_width=2, color = "navy", legend_label="real")
         p_pos.line(np.array(dim.pos_array()), np.imag(arr.pos_array().values), line_width=2, color = "firebrick", legend_label="imag")
-        p_pos.title.text = f"{data_name or 'FFTArray values'} shown in position space"
+        p_pos.title.text = f"{data_name or 'FFTArray values'} shown in position space" # type: ignore
 
         p_freq = figure(width=450, height=400, x_axis_label = f"{dim.name} freq coordinate", min_border=50)
         p_freq.line(np.array(dim.freq_array()), np.real(arr.freq_array().values), line_width=2, color = "navy", legend_label="real")
         p_freq.line(np.array(dim.freq_array()), np.imag(arr.freq_array().values), line_width=2, color = "firebrick", legend_label="imag")
-        p_freq.title.text = f"{data_name or 'FFTArray values'} shown in frequency space"
+        p_freq.title.text = f"{data_name or 'FFTArray values'} shown in frequency space" # type: ignore
 
         plot = row([p_pos, p_freq], sizing_mode="stretch_width") # type: ignore
     else:
