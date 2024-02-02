b1d933ca4dfcea325c71f99203025a78137821e4
Christian Struckmann <christian.struckmann@gmx.de>
2024-02-02 16:59:59 +0100

    Update jax test

--- fftarray/tests/test_jax.py
+++ fftarray/tests/test_jax.py
@@ -6,8 +6,9 @@ from fftarray import FFTDimension
 from jax import config
 config.update("jax_enable_x64", True)
 
+import jax.numpy as jnp
+from jax import random
+
 def test_jax_fp64():
-    import jax.numpy as jnp
-    from jax import random
     x = random.uniform(random.PRNGKey(0), (1000,), dtype=jnp.float64)
     assert x.dtype == jnp.float64
