a00d34a7a010d3a8183983ea0c1a29cb92339a1c
SJSeckmeyer <105303792+SJSeckmeyer@users.noreply.github.com>
2024-02-08 16:50:14 +0100

    Rename runslow to include-slow
    Co-authored-by: Christian Struckmann <56967696+cstruckmann@users.noreply.github.com>

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
+        "--include-slow", action="store_true", default=False, help="run slow tests"
     )
 
 
@@ -13,10 +13,10 @@ def pytest_configure(config):
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
+    if config.getoption("--include-slow"):
+        # --include-slow given in cli: do not skip slow tests
         return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+    skip_slow = pytest.mark.skip(reason="need --include-slow option to run")
     for item in items:
         if "slow" in item.keywords:
             item.add_marker(skip_slow)
