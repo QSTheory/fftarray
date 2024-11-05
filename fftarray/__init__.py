411019f0a3e1960daeba04073e99b3f0b5040adf
Stefan Seckmeyer <seckmeyer@iqo.uni-hannvoer.de>
2024-11-05 09:33:24 +0100

    Add dim_from_constraints to top namespace.
    This import works conditionally to keep the t3-solver optional.
    
    stack-info: PR: https://github.com/QSTheory/fftarray/pull/168, branch: SJSeckmeyer/stack/21

--- fftarray/__init__.py
+++ fftarray/__init__.py
@@ -28,9 +28,37 @@ Example:
    >>> from fftarray import FFTArray, FFTDimension
 
 """
+
 from .fft_dimension import FFTDimension
 from .fft_array import (
     FFTArray, Space, set_default_backend, get_default_backend,
     set_default_eager, get_default_eager, default_backend, default_eager
 )
+
 from .tools import shift_frequency, shift_position
+
+
+from typing import Optional, Literal, Union, List
+try:
+   from .constraint_solver import get_fft_dim_from_constraints as dim_from_constraints
+except ModuleNotFoundError:
+   def dim_from_constraints(
+         name: str,
+         *,
+         n: Union[int, Literal["power_of_two", "even"]] = "power_of_two",
+         d_pos: Optional[float] = None,
+         d_freq: Optional[float] = None,
+         pos_min: Optional[float] = None,
+         pos_max: Optional[float] = None,
+         pos_middle: Optional[float] = None,
+         pos_extent: Optional[float] = None,
+         freq_min: Optional[float] = None,
+         freq_max: Optional[float] = None,
+         freq_extent: Optional[float] = None,
+         freq_middle: Optional[float] = None,
+         loose_params: Optional[Union[str, List[str]]] = None,
+         dynamically_traced_coords: bool = True,
+    ) -> FFTDimension:
+      raise ModuleNotFoundError("You need to install `fftarray[helpers]` to use the constraint solver.")
+
+
