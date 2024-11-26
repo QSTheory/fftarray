a1e9696b09455efab1c07bb9b2500be70e35b0b3
Stefan Seckmeyer <seckmeyer@iqo.uni-hannover.de>
2024-11-26 11:23:32 +0100

    Test notebooks with all Array API implementations
    This is now a much more straight-forward implementation of loading and executing the notebooks.
    
    stack-info: PR: https://github.com/QSTheory/fftarray/pull/186, branch: SJSeckmeyer/stack/29

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -1,107 +1,47 @@
 # Runs all the notebooks as parts of the tests.
-# from https://jupyter-notebook.readthedocs.io/en/5.7.6/examples/Notebook/Importing%20Notebooks.html
+# originally based on https://jupyter-notebook.readthedocs.io/en/5.7.6/examples/Notebook/Importing%20Notebooks.html
 
-import io, os, sys, types
-from importlib.machinery import ModuleSpec
+import io, types
+import pytest
 
 from IPython import get_ipython
 from nbformat import read
 from IPython.core.interactiveshell import InteractiveShell
 
-def find_notebook(fullname, path=None):
-    """find a notebook, given its fully qualified name and an optional path
+from fftarray.tests.helpers import XPS
+import fftarray as fa
 
-    This turns "foo.bar" into "foo/bar.ipynb"
-    and tries turning "Foo_Bar" into "Foo Bar" if Foo_Bar
-    does not exist.
-    """
-    name = fullname.rsplit('.', 1)[-1]
-    if not path:
-        path = ['']
-    for d in path:
-        nb_path = os.path.join(d, name + ".ipynb")
-        if os.path.isfile(nb_path):
-            return nb_path
-        # let import Notebook_Name find "Notebook Name.ipynb"
-        nb_path = nb_path.replace("_", " ")
-        if os.path.isfile(nb_path):
-            return nb_path
+@pytest.mark.parametrize("xp", XPS)
+@pytest.mark.parametrize("nb_path", [
+    "Gaussians",
+    "multi_dimensional",
+])
+def test_notebooks(xp, nb_path):
+    # load the notebook object
+    with io.open(f"examples/{nb_path}.ipynb", 'r', encoding='utf-8') as f:
+        nb = read(f, 4)
 
+    shell = InteractiveShell.instance()
+    # create the module
+    mod = types.ModuleType("notebook")
+    mod.__file__ = nb_path
+    mod.__dict__['get_ipython'] = get_ipython
+    mod.__dict__["xp"] = xp
 
+    # extra work to ensure that magics that would affect the user_ns
+    # actually affect the notebook module's ns
+    save_user_ns = shell.user_ns
+    shell.user_ns = mod.__dict__
 
-class NotebookLoader(object):
-    """Module Loader for Jupyter Notebooks"""
-    def __init__(self, path=None):
-        self.shell = InteractiveShell.instance()
-        self.path = path
-
-    def load_module(self, fullname):
-        """import a notebook as a module"""
-        path = find_notebook(fullname, self.path)
-
-        print("importing Jupyter notebook from %s" % path)
-
-        # load the notebook object
-        with io.open(path, 'r', encoding='utf-8') as f:
-            nb = read(f, 4)
-
-
-        # create the module and add it to sys.modules
-        # if name in sys.modules:
-        #    return sys.modules[name]
-        mod = types.ModuleType(fullname)
-        mod.__file__ = path
-        mod.__loader__ = self
-        mod.__dict__['get_ipython'] = get_ipython
-        sys.modules[fullname] = mod
-
-        # extra work to ensure that magics that would affect the user_ns
-        # actually affect the notebook module's ns
-        save_user_ns = self.shell.user_ns
-        self.shell.user_ns = mod.__dict__
-
-        try:
-          for cell in nb.cells:
+    try:
+        for cell in nb.cells:
             if cell.cell_type == 'code':
-                # transform the input to executable Python
-                code = self.shell.input_transformer_manager.transform_cell(cell.source)
-                # run the code in themodule
-                exec(code, mod.__dict__)
-        finally:
-            self.shell.user_ns = save_user_ns
-        return mod
-
-
-
-class NotebookFinder(object):
-    """Module finder that locates Jupyter Notebooks"""
-    def __init__(self):
-        self.loaders = {}
-
-    def find_spec(self, fullname, path, target=None):
-        nb_path = find_notebook(fullname, path)
-        if not nb_path:
-            return
-
-        key = path
-        if path:
-            # lists aren't hashable
-            key = os.path.sep.join(path)
-        key += fullname
-
-        if key not in self.loaders:
-            self.loaders[key] = ModuleSpec(
-                name=fullname,
-                loader=NotebookLoader(path),
-            )
-        return self.loaders[key]
-
-sys.meta_path.append(NotebookFinder()) # type: ignore
-
-def test_gaussians():
-    # The import statements automatically evaluate all cells.
-    from examples import Gaussians
-
-def test_multi_dimensional():
-    # The import statements automatically evaluate all cells.
-    from examples import multi_dimensional
+                # Set the Array API implementation to test with.
+                with fa.default_xp(xp):
+                    # transform the input to executable Python
+                    code = shell.input_transformer_manager.transform_cell(cell.source)
+                    # run the code in themodule
+                    exec(code, mod.__dict__)
+    finally:
+        shell.user_ns = save_user_ns
+    return mod
