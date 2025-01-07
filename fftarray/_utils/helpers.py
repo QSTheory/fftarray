19b22082007f3caf0568712a8ce8f39c27a76e05
Stefan Seckmeyer <105303792+SJSeckmeyer@users.noreply.github.com>
2025-01-07 12:29:59 +0100

    add `norm_space` (#203)
    This replaces `norm_param` to provide better error messages and more precise typing.
    The other of `norm_param` will be adjusted similarly.
    
    Co-authored-by: Gabriel Müller <51076825+gabmueller@users.noreply.github.com>

--- fftarray/_utils/helpers.py
+++ fftarray/_utils/helpers.py
@@ -1,6 +1,39 @@
-from typing import TypeVar, Union, Iterable, Tuple
+from typing import TypeVar, Union, Iterable, Tuple, get_args, cast
+
+from ..space import Space
 
 T = TypeVar("T")
+
+
+space_args = get_args(Space)
+def _check_space(x: str) -> Space:
+    if x not in space_args:
+        raise ValueError(f"Only valid values for space are: {space_args}. Got passed '{x}'.")
+    return cast(Space, x)
+
+def norm_space(val: Union[Space, Iterable[Space]], n: int) -> Tuple[Space, ...]:
+    """
+       `val` has to be immutable.
+    """
+
+    if isinstance(val, str):
+        return (_check_space(val),)*n
+
+    try:
+        input_list = list(val)
+    except(TypeError) as e:
+        raise TypeError(
+            f"Got passed '{val}' as space which raised an error on iteration."
+        ) from e
+
+    res_tuple = tuple(_check_space(x) for x in input_list)
+
+    if len(res_tuple) != n:
+        raise ValueError(
+            f"Got passed '{val}' as space which has length {len(res_tuple)} "
+            + f"but there are {n} dimensions."
+        )
+    return res_tuple
 
 def norm_param(val: Union[T, Iterable[T]], n: int, types) -> Tuple[T, ...]:
     """
