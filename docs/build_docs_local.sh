3f167503b686fb5456398b9027216dd6662ef13d
Christian Struckmann <struckmann@iqo.uni-hannover.de>
2025-02-25 14:59:39 +0100

    Update local docs build file

--- docs/build_docs_local.sh
+++ docs/build_docs_local.sh
@@ -1,4 +1,6 @@
+set -e  # Stop on first error
 python helpers/generate_versions.py
 python helpers/create_nblinks.py
 python helpers/parse_classes.py
+rm -rf source/api/generated/*
 sphinx-build --color -b html source build/html/local -v
