4a5432c93f738bb0a8cc87963f77bcfe77830a32
Stefan Seckmeyer <seckmeyer@iqo.uni-hannover.de>
2025-03-19 11:28:35 +0100

    Fix `run_all_checks_all_python_versions.sh` Adds a little comment, explainign what it does. The doc check was not adjusted when changing the doc build process. Additionally also add the forgotten checknumpy125 environment.

--- run_all_checks_all_python_versions.sh
+++ run_all_checks_all_python_versions.sh
@@ -1,4 +1,6 @@
-pixi run -e doc doc  && continue || exit 1
-for pyenv in check310 check311 check312 check313; do
+# This script aims to run the whole test suite locally to prevent surprises in CI.
+pixi run -e doc doc_local && continue || exit 1
+pixi run -e doc doc_all_versions && continue || exit 1
+for pyenv in checknumpy125 check310 check311 check312 check313; do
   (command pixi run -e $pyenv check_all) && continue || exit 1
 done
