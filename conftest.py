40f0c153ae58188aaae4fa9e78a26e8a1ed5cafd
Stefan Seckmeyer <seckmeyer@iqo.uni-hannover.de>
2025-07-17 19:15:23 +0200

    Fix math rendering on pypi
    Remove coloneqq from README.md because it is not supported.
    Add an rst version of the README, because only that supports proper math rendering on pypi.
    But the README.md is necessary for math rendering on github.
    So this also adds a covnersion command and a test that the generated rst matches the current one.
    Since the test requires pandoc, it is skippable for the pip installation test.
    
    stack-info: PR: https://github.com/QSTheory/fftarray/pull/302, branch: SJSeckmeyer/stack/1

--- conftest.py
+++ conftest.py
@@ -4,19 +4,27 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--include-slow", action="store_true", default=False, help="run slow tests"
+        "--include-slow", action="store_true", default=False, help="run slow tests",
+    )
+    parser.addoption(
+        "--skip-pandoc", action="store_true", default=False, help="skip tests which require pandoc",
     )
 
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
+    config.addinivalue_line("markers", "pandoc: mark test which requires pandoc")
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--include-slow"):
-        # --include-slow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --include-slow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
+    if not config.getoption("--include-slow"):
+        skip_slow = pytest.mark.skip(reason="need --include-slow option to run")
+        for item in items:
+            if "slow" in item.keywords:
+                item.add_marker(skip_slow)
+
+    if config.getoption("--skip-pandoc"):
+        skip_pandoc: pytest.MarkDecorator = pytest.mark.skip(reason="need to omit --skip-pandoc option to run")
+        for item in items:
+            if "pandoc" in item.keywords:
+                item.add_marker(skip_pandoc)
